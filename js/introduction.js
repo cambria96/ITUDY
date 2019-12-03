f981cdf9e9788bf0310a6aa960c174f471eca5a7
eunchang <affinity96@ajou.ac.kr>
2019-12-03 18:14:08 +0900

    뀨

--- js/introduction.js
+++ js/introduction.js
@@ -21,4 +21,14 @@ $(document).ready(function(){
             $(".mainTitle").removeClass("active");
         }
       });
+
+    $('ul.tabs li').click(function(){
+        var tab_id = $(this).attr('data-tab');
+
+        $('ul.tabs li').removeClass('current');
+        $('.tab-content').removeClass('current');
+
+        $(this).addClass('current');
+        $("#"+tab_id).addClass('current');
+    })
 })
